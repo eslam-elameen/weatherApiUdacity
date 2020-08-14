ac75f75b70f0b56d3c9bb4b93134d45bac3a2a1a
eslam-elameen <e.elameen92@gmail.com>
2020-08-14 05:31:55 +0200

    creating weather journal app project

---
+++ server.js
@@ -0,0 +1,52 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {
+    tempreature: '',
+    date: ' ',
+    userReasponse: ' '
+};
+
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+const bodyParser = require('body-parser');
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+const port = 8000;
+
+function listening() {
+    console.log('server running');
+    console.log(`running on local : ${port}`);
+
+}
+// Setup Server
+const server = app.listen(port, listening);
+
+
+
+// get rout
+app.get('/getData', receiveData);
+
+function receiveData(req, response) {
+    response.send(projectData);
+}
+
+// post rout 
+app.post('/UpdateData', sendData);
+
+function sendData(req, res) {
+    console.log(req.body);
+    projectData.userReasponse = req.body.userReasponse;
+    projectData.tempreature = req.body.tempreature;
+    projectData.date = req.body.date;
+
+    res.send('Success')
+}
---
+++ website/app.js
@@ -0,0 +1,78 @@
+/* Global Variables */
+const key = '53ab2019377dcd8259f3e7f3863802ce'
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+// click function to generate click button
+document.getElementById('generate').addEventListener('click', submitData);
+
+function submitData() {
+    const zipCode = document.getElementById('zip').value;
+    const userRes = document.getElementById('feelings').value;
+    const baseURL = `http://api.openweathermap.org/data/2.5/weather?q=${zipCode}&APPID=53ab2019377dcd8259f3e7f3863802ce`;
+
+    // to get data from api using get method
+    getWeather(baseURL).then((res) => {
+        // to update data which coming from getWeather function
+        postWeather('http://localhost:8000/UpdateData', { tempreature: res.main.temp, date: newDate, userReasponse: userRes }).then((r) => {
+            // to get data from server api 
+            updateDta()
+        })
+    })
+}
+
+
+// get weather
+const getWeather = async(baseURL, key, submitData) => {
+    const res = await fetch(baseURL, key, submitData);
+    try {
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+
+// post weather
+const postWeather = async(url, data = {}) => {
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(data)
+    })
+    try {
+        const editData = await res.json();
+        return editData
+    } catch (error) {
+        console.log('error', error);
+    }
+}
+
+// fetching data from server side and demonstrate them in DOM 
+const updateDta = async() => {
+    const data = await fetch('http://localhost:8000/getData')
+    const updatedData = await data.json();
+    console.log('get Data ', updatedData);
+    const uiDate = document.getElementById('date');
+    const temp = document.getElementById('temp');
+    const content = document.getElementById('content');
+
+    // ui data
+    const dateContent = document.createElement('p');
+    dateContent.textContent = 'Date : ' + updatedData.date;
+    uiDate.appendChild(dateContent);
+
+    // temp data
+    const tempContent = document.createElement('p');
+    tempContent.textContent = 'Tempreature : ' + updatedData.tempreature;
+    temp.appendChild(tempContent);
+
+    // content data
+    const contentData = document.createElement('p');
+    contentData.textContent = 'Feeling : ' + updatedData.userReasponse;
+    content.appendChild(contentData);
+}
